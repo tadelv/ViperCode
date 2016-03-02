3dd4918c80befc55fb9cf2d38f5af5296bee09ef
Sameh <mabrouksameh@gmail.com>
2016-03-02 23:47:07 +0200

    Initial Commit

---
+++ ViperCode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViperCode
+//
+//  Created by Sameh Mabrouk on 3/2/16.
+//  Copyright © 2016 smapps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ViperCode/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  ViperCode
+//
+//  Created by Sameh Mabrouk on 3/2/16.
+//  Copyright © 2016 smapps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
